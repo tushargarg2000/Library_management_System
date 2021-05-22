c55f14fb38ec359755ba3a00680cf461ee96e018
Tushar Garg <tushargarg0110@gmail.com>
2021-05-22 23:10:30 +0530

    swagger dependencies also added

--- Library/src/main/java/com/example/student/Library/LibraryApplication.java
+++ Library/src/main/java/com/example/student/Library/LibraryApplication.java
@@ -11,9 +11,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
+@EnableSwagger2
 public class LibraryApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
